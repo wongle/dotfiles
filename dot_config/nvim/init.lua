730de2221099330190dccaa942be3474ba19658f
Tim Wong <124337+wongle@users.noreply.github.com>
2023-04-24 14:08:19 +1000

    update neovim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -100,11 +100,34 @@ require("lazy").setup({
     -- See `:help indent_blankline.txt`
     opts = {
         char = '┊',
+        -- char = "│",
         show_trailing_blankline_indent = true,
         -- space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
+        show_current_context = false,
+        -- show_current_context_start = true,
     },
+  },
+
+  {
+    "echasnovski/mini.indentscope",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+    config = function(_, opts)
+      require("mini.indentscope").setup(opts)
+    end,
   },
 
   -- "gc" to comment visual regions/lines
