ce0d5eb1cf351b09e51c9b391f99d538a2c68133
Tim Wong <124337+wongle@users.noreply.github.com>
2025-05-23 06:37:47 +0000

    feat: update fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,20 @@
+# Remove welcome message
+set -g fish_greeting
+
+# XDG Base Directories
+set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_STATE_HOME "$HOME/.local/state"
+set -x XDG_CACHE_HOME "$HOME/.cache"
+
+# Editor
+set -x EDITOR nvim
+
+# 1Password Shell Plugins
 source ~/.config/op/plugins.sh
 
+# Mise
 mise activate fish | source
 
+# Starship
 starship init fish | source
