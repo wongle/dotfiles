ce2a89465588b102809e05d8501001042b7ae2c0
Tim Wong <124337+wongle@users.noreply.github.com>
2025-06-18 22:36:54 +1000

    feat(nvim): tweak theme

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,18 @@
 return {
   {
+    "catppuccin",
+    opts = {
+      custom_highlights = function(colors)
+        return {
+          CursorLineNr = { fg = colors.peach, bold = true },
+        }
+      end,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
-    }
-  }
+    },
+  },
 }
